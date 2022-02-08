a4c42cd204c59e517842c8547741042f71157ed7
fvn70 <fvn_reg@mail.ru>
2022-02-08 22:05:37 +0700

    Stage 3/8

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,5 +1,7 @@
-# Write your code here
-word = "python"
+import random
+
+words = ['python', 'java', 'kotlin', 'javascript']
+word = random.choice(words)
 print("H A N G M A N")
 if input("Guess the word: ") == word:
     print("You survived!")
