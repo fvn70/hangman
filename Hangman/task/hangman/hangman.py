d5e06dcf3c92f580ae3ac254ed63e400900e5af8
fvn70 <fvn_reg@mail.ru>
2022-02-09 12:18:14 +0700

    Stage 6/8

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,19 +1,29 @@
 import random
 
+lives = 8
 words = ['python', 'java', 'kotlin', 'javascript']
 word = random.choice(words)
 puzzle = '-' * len(word)
 print("H A N G M A N")
-for _ in range(8):
+while lives:
     print()
     print(puzzle)
     ch = input("Input a letter: ")
     if ch in word:
-        for i in range(len(word)):
-            if word[i] == ch:
-                puzzle = puzzle[:i] + ch + puzzle[i + 1:]
+        if ch in puzzle:
+            print("No improvements")
+            lives -= 1
+        else:
+            for i in range(len(word)):
+                if word[i] == ch:
+                    puzzle = puzzle[:i] + ch + puzzle[i + 1:]
+            if puzzle == word:
+                print("You guessed the word!")
+                print("You survived!")
+                break
     else:
+        lives -= 1
         print("That letter doesn't appear in the word")
 
-print("\nThanks for playing!")
-print("We'll see how well you did in the next stage")
+if not lives:
+    print("You lost!")
