6c22d12e0a90ede313f36de77f5d06c5d35ff4cd
fvn70 <fvn_reg@mail.ru>
2022-02-09 11:20:14 +0700

    Stage 4/8

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -3,7 +3,7 @@ import random
 words = ['python', 'java', 'kotlin', 'javascript']
 word = random.choice(words)
 print("H A N G M A N")
-if input("Guess the word: ") == word:
+if input(f"Guess the word {word[:3]}{'-' * (len(word) - 3)}: ") == word:
     print("You survived!")
 else:
     print("You lost!")
