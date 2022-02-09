aaf7a3b36073579fe2fcd1283f3ee2925d7255ce
fvn70 <fvn_reg@mail.ru>
2022-02-09 13:46:18 +0700

    Stage 7/8

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,29 +1,53 @@
 import random
+import re
 
-lives = 8
-words = ['python', 'java', 'kotlin', 'javascript']
-word = random.choice(words)
-puzzle = '-' * len(word)
-print("H A N G M A N")
-while lives:
-    print()
-    print(puzzle)
-    ch = input("Input a letter: ")
-    if ch in word:
-        if ch in puzzle:
-            print("No improvements")
-            lives -= 1
-        else:
-            for i in range(len(word)):
-                if word[i] == ch:
-                    puzzle = puzzle[:i] + ch + puzzle[i + 1:]
-            if puzzle == word:
-                print("You guessed the word!")
-                print("You survived!")
+class Game:
+
+    lives = 8
+    words = ['python', 'java', 'kotlin', 'javascript']
+    word = random.choice(words)
+    puzzle = '-' * len(word)
+    w_set = set()
+
+    def read_char(self):
+        while self.lives:
+            print()
+            print(self.puzzle)
+            ch = input("Input a letter: ")
+            if len(ch) != 1:
+                print("You should input a single letter")
+            elif not re.match('[a-z]', ch):
+                print("Please enter a lowercase English letter")
+            elif ch in self.w_set:
+                print("You've already guessed this letter")
+            else:
+                self.w_set.add(ch)
                 break
-    else:
-        lives -= 1
-        print("That letter doesn't appear in the word")
+        return ch
 
-if not lives:
-    print("You lost!")
+    def play(self):
+
+        print("H A N G M A N")
+        while self.lives:
+            ch = self.read_char()
+            if ch in self.word:
+                if ch in self.puzzle:
+                    print("No improvements")
+                    self.lives -= 1
+                else:
+                    for i in range(len(self.word)):
+                        if self.word[i] == ch:
+                            self.puzzle = self.puzzle[:i] + ch + self.puzzle[i + 1:]
+                    if self.puzzle == self.word:
+                        print("You guessed the word!")
+                        print("You survived!")
+                        break
+            else:
+                self.lives -= 1
+                print("That letter doesn't appear in the word")
+
+        if not self.lives:
+            print("You lost!")
+
+game = Game()
+game.play()
